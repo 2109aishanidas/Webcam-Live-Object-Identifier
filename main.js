966269beccfd8b568c3e169f5fe4a702052bdc03
2109aishanidas <75248091+2109aishanidas@users.noreply.github.com>
2021-03-12 18:04:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 video=""
-function preload(){
-    video=createCapture(VIDEO)
-    video.hide()
-}
+status=""
+objects=""
+
 function setup(){
     canvas=createCanvas(480,380)
     canvas.center()
+    video=createVideo('video.mp4')
+    video.hide()
 }
 function draw(){
     image(video,0,0,480,380)
+    if(status !=""){
+        objectDetector.detect(video,gotResult)
+        for(i=0; i<objects.length; i++ ){
+            document.getElementById("status").innerHTML="Status : Objects Detected"
+            document.getElementById("number_of_objects").innerHTML="Number of objects detected are : "+objects.length
+            fill("#FF0000")
+            percent=floor(objects[i].confidence*100)
+            text(objects[i].lable+" "+percent+"%"+objects[i].x+15,objects[i].y+15)
+            nofill()
+            stroke("#FF0000")
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height )
+        }
+    }
+    function gotResult(error,results){
+        if(error){
+            console.log
+        }
+        console.log(results)
+        objects=results
+    }
 }
 function start(){
     objectDetector=ml5.objectDetector('cocossd',modelLoaded)
@@ -16,4 +37,6 @@ function start(){
 }
 function modelLoaded(){
     console.log("Model Loaded !")
+    status=true
+    
 }
